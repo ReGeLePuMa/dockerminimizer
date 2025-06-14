abaed9c41378d77e757b64fb1cf70930ff755bc1
ReGeLePuMa <scpetrea@gmail.com>
2025-06-14 16:30:07 +0300

    Switch to cobra for cli arguments
    Signed-off-by: ReGeLePuMa <scpetrea@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-	"flag"
+	"github.com/spf13/cobra"
 
 	"github.com/regelepuma/dockerminimizer"
 	"github.com/regelepuma/dockerminimizer/types"
 )
 
-func parseArgs() types.Args {
-	dockerfile := flag.String("file", "./Dockerfile", "Path to the Dockerfile")
-	image := flag.String("image", "", "Name of the Docker image")
-	maxLimit := flag.Int("max_limit", 10, "Maximum number of retries")
-	debug := flag.Bool("debug", false, "Enable debug mode")
-	timeout := flag.Int("timeout", 30, "How long the container should run before being declared healthy")
-	stracePath := flag.String("strace_path", "/usr/local/bin/strace", "Path to the statically linked strace binary")
-	binarySearch := flag.Bool("binary_search", true, "Continue with binary search if dynamic analysis fails")
-
-	flag.Parse()
-
-	return types.Args{
-		Dockerfile:   *dockerfile,
-		Image:        *image,
-		MaxLimit:     *maxLimit,
-		Debug:        *debug,
-		Timeout:      *timeout,
-		StracePath:   *stracePath,
-		BinarySearch: *binarySearch,
+func parseArgs(runFunc func(args types.Args)) *cobra.Command {
+	var args types.Args
+	cmd := &cobra.Command{
+		Use:   "dockerminimizer",
+		Short: "A tool to minimize Dockerfiles by determining the dependencies of the containerized application",
+		Run: func(cmd *cobra.Command, _ []string) {
+			runFunc(args)
+		},
 	}
+
+	cmd.Flags().StringVarP(&args.Dockerfile, "file", "f", "./Dockerfile", "Path to the Dockerfile")
+	cmd.Flags().StringVarP(&args.Image, "image", "i", "", "Name of the Docker image")
+	cmd.Flags().IntVar(&args.MaxLimit, "max_limit", 10, "Number of binary search steps")
+	cmd.Flags().BoolVar(&args.Debug, "debug", false, "Enable debug mode")
+	cmd.Flags().IntVar(&args.Timeout, "timeout", 30, "How long the container should run before being declared healthy")
+	cmd.Flags().StringVar(&args.StracePath, "strace_path", "/usr/local/bin/strace", "Path to the statically linked strace binary")
+	cmd.Flags().BoolVar(&args.BinarySearch, "binary_search", true, "Continue with binary search if dynamic analysis fails")
+	return cmd
 }
 
 func main() {
-	dockerminimizer.Run(parseArgs())
+	err := parseArgs(dockerminimizer.Run).Execute()
+	if err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -7,13 +7,16 @@ require (
 	github.com/moby/buildkit v0.21.0
 	github.com/samber/lo v1.50.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.9.1
 )
 
 require (
 	github.com/containerd/typeurl/v2 v2.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/sys v0.32.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
